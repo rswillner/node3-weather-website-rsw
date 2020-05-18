0be77efd7f060b59c406fe00aad13843e96e35a3
Robert Willner <rswillner@gmail.com>
2020-05-18 16:58:23 -0400

    Removed unneeded console.log stmt

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('javascript module app.js executed');
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
